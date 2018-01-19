e794bb09ae875897f892aa4ab11d4bbce79eed64
callumpoole <14567682+callumpoole@users.noreply.github.com>
2018-01-20 01:34:03 +0700

    Join server menu almost functional

---
+++ Assets/Scripts/JoinGame.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.Types;
+using System.Collections.Generic;
+using System;
+
+public class JoinGame : MonoBehaviour {
+
+    List<GameObject> roomList = new List<GameObject>();
+    private NetworkManager networkManager;
+    [SerializeField]
+    private Text status;
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+    [SerializeField]
+    private GameObject roomListParent;
+
+    void Start () {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null) {
+            networkManager.StartMatchMaker();
+        }
+        RefreshRoomList();
+	}
+
+
+
+    public void RefreshRoomList() {
+        ClearRoomList();
+        networkManager.matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+        status.text = "Loading...";
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches) {
+        status.text = "";
+        if (!success) {
+            status.text = "ERROR: Couldn't get room list.";
+            return;
+        }  
+        if (matches.Count == 0) //No matches found
+            status.text = "No Matches Found.";
+        
+        foreach (MatchInfoSnapshot match in matches) {
+            GameObject roomListItemGO = Instantiate(roomListItemPrefab);
+            roomListItemGO.transform.SetParent(roomListParent.transform);
+
+            RoomListItem roomItem = roomListItemGO.GetComponent<RoomListItem>();
+            if (roomItem != null)
+                roomItem.Setup(match, JoinRoom);
+
+            roomList.Add(roomListItemGO);
+        } 
+    }
+
+    void ClearRoomList() {
+        for (int i = 0; i < roomList.Count; i++) 
+            Destroy(roomList[i]);
+        roomList.Clear();
+      
+    }
+
+    public void JoinRoom(MatchInfoSnapshot match) {
+        networkManager.matchMaker.JoinMatch(match.networkId, "", "", "", 0, 0, NetworkManager.OnMatchJoined);
+        ClearRoomList();
+        status.text = "Joining...";
+    }
+
+   
+}
---
+++ Assets/Scripts/RoomListItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking.Match;
+
+public class RoomListItem : MonoBehaviour {
+
+    public delegate void JoinRoomDelegate(MatchInfoSnapshot match);
+    private JoinRoomDelegate joinRoomCallback;
+
+    [SerializeField]
+    private Text roomNameText;
+
+    private MatchInfoSnapshot match;
+
+    public void Setup(MatchInfoSnapshot _match, JoinRoomDelegate jrc) {
+        match = _match;
+        joinRoomCallback = jrc;
+        roomNameText.text = match.name + " (" + match.currentSize + "/" + match.maxSize + ")";
+    }
+
+    public void JoinRoom() {
+        joinRoomCallback.Invoke(match); 
+    }
+}
